9e473a90412f0a0bb80a70efe37e4dd5ab0ddbfa
onglijin <onglijinlj@gmail.com>
2020-10-26 21:48:49 +0800

    Remove unnecessary whitespace

--- src/main/java/tp/acecs2103/model/task/Admin.java
+++ src/main/java/tp/acecs2103/model/task/Admin.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Admin extends Task {
 
     public Admin(Index index, WeekNumber weekNumber, Description description,
-                 OfficialDeadline officialDeadline, 
+                 OfficialDeadline officialDeadline,
                  CustomizedDeadline customizedDeadline, Remark remark) {
         super(index, weekNumber, description, officialDeadline, customizedDeadline, remark);
     }
