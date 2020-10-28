5b65dfc31f236f9027e04521a7b7ad0dd0173f5a
JinYuze <1072217974@qq,com>
2020-10-29 00:15:56 +0800

    Finish Debug, new Command: Done, Undone, Filter

---
+++ src/main/java/tp/acecs2103/logic/parser/DoneCommandParser.java
@@ -0,0 +1,20 @@
+package tp.acecs2103.logic.parser;
+
+import tp.acecs2103.commons.core.Messages;
+import tp.acecs2103.logic.commands.DeleteCommand;
+import tp.acecs2103.logic.commands.DoneCommand;
+import tp.acecs2103.logic.parser.exceptions.ParseException;
+import tp.acecs2103.model.task.Index;
+
+public class DoneCommandParser implements Parser<DoneCommand> {
+    public DoneCommand parse(String args) throws ParseException {
+        try {
+            String indexParsed = ParserUtil.parseIndex(args);
+            Index index = new Index(indexParsed);
+            return new DoneCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
