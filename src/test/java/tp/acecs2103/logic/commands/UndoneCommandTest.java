73d7556e5d80138e35794c6cd991b458ee15f082
Zhang Yunjie <e0421414@u.nus.edu>
2020-11-08 07:50:38 +0800

    Add test codes for undone command

--- src/test/java/tp/acecs2103/logic/commands/UndoneCommandTest.java
+++ src/test/java/tp/acecs2103/logic/commands/UndoneCommandTest.java
@@ -8,29 +8,29 @@ import tp.acecs2103.model.UserPrefs;
 import tp.acecs2103.model.task.*;
 import tp.acecs2103.testutil.TypicalTasks;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static tp.acecs2103.testutil.Assert.assertThrows;
 
-class DoneCommandTest {
+class UndoneCommandTest {
     @Test
     public void execute_validIndex_success() {
         Model model = new ModelManager(TypicalTasks.getTypicalTaskList(), new UserPrefs());
 
         Index index = new Index("0301");
-        DoneCommand doneCommand = new DoneCommand(index);
+        UndoneCommand undoneCommand = new UndoneCommand(index);
         Task task = model.getTaskList().getTask(index);
+        task.markAsDone();
 
         CommandResult commandResult = null;
         try {
-            commandResult= doneCommand.execute(model);
+            commandResult= undoneCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
 
-        String expectedMessage = String.format(DoneCommand.MESSAGE_DONE_TASK_SUCCESS, task);
+        String expectedMessage = String.format(UndoneCommand.MESSAGE_DONE_TASK_SUCCESS, task);
         assertEquals(expectedMessage, commandResult.getFeedbackToUser());
-        assertTrue(task.isDone());
+        assertTrue(!task.isDone());
     }
 
     @Test
@@ -38,28 +38,28 @@ class DoneCommandTest {
         Model model = new ModelManager(TypicalTasks.getTypicalTaskList(), new UserPrefs());
 
         Index index = new Index("0203");
-        DoneCommand doneCommand = new DoneCommand(index);
+        UndoneCommand doneCommand = new UndoneCommand(index);
 
         assertThrows(CommandException.class, () -> doneCommand.execute(model));
     }
 
     @Test
-    public void execute_invalidIndex_DoneTask_fail() {
+    public void execute_invalidIndex_UndoneTask_fail() {
         Model model = new ModelManager(TypicalTasks.getTypicalTaskList(), new UserPrefs());
 
         Index index = new Index("0101");
         Task task = model.getTaskList().getTask(index);
-        task.markAsDone();
-        DoneCommand doneCommand = new DoneCommand(index);
+        UndoneCommand doneCommand = new UndoneCommand(index);
 
         assertThrows(CommandException.class, () -> doneCommand.execute(model));
     }
 
+    @Test
     public void equal_null_returnTrue() {
         Index index = new Index("0101");
 
-        DoneCommand doneCommand = new DoneCommand(index);
-        DoneCommand doneCommand1 = new DoneCommand(index);
+        UndoneCommand doneCommand = new UndoneCommand(index);
+        UndoneCommand doneCommand1 = new UndoneCommand(index);
 
         assertTrue(doneCommand.equals(doneCommand1));
     }
@@ -69,8 +69,8 @@ class DoneCommandTest {
         Index index = new Index("0101");
         Index index1 = new Index("0102");
 
-        DoneCommand doneCommand = new DoneCommand(index);
-        DoneCommand doneCommand1 = new DoneCommand(index1);
+        UndoneCommand doneCommand = new UndoneCommand(index);
+        UndoneCommand doneCommand1 = new UndoneCommand(index1);
 
         assertFalse(doneCommand.equals(doneCommand1));
         assertFalse(doneCommand.equals(null));
